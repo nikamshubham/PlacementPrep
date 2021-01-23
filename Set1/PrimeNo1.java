75147f1983216de24380430e992ae9b637f3c2ac
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-01-23 10:17:46 +0530

    Prime no using loop

--- Set1/PrimeNo1.java
+++ Set1/PrimeNo1.java
@@ -1,9 +1,24 @@
 package Set1;
 
+import java.util.Scanner;
+
 public class PrimeNo1 {
 
 	public static void main(String[] args) {
-		 
+		//WAP to Check Prime Number using loop?
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number - ");
+		int no = sc.nextInt();
+		int count=0,i;
+		for (i = 2; i <= no-1; i++) {
+			if(no%i==0)
+				count++;
+		}
+		if (count==0) {
+			System.out.println("Number is prime");
+		} else {
+			System.out.println("Number is not prime");
+		}
 		
 	}
 
