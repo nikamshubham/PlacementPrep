b7e23d0f7299519d43a6b1ed1afed220967eb3fa
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-01-26 12:28:49 +0530

    Print the following pattern
    1
    1 2
    1 2 3
    1 2 3 4
    1 2 3 4 5
    1 2 3 4 5 6

---
+++ Pattern/Pattern2.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+//				if(j<=i) {
+					System.out.print(i);
+//				}else {
+//					System.out.print(" ");
+//				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
