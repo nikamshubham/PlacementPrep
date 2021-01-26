6ea0163605b9498a6da91516d1cc5f3987211158
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-01-26 10:15:27 +0530

    Update CheckLeapYear.java

--- Set1/CheckLeapYear.java
+++ Set1/CheckLeapYear.java
@@ -9,6 +9,10 @@ public class CheckLeapYear {
 		Scanner sc = new Scanner(System.in);
 		int yr = sc.nextInt();
 		
+ /* Leap Year Conditions
+ * 1. Year should be divisible by 4
+ * 2. If it is divisible by 100 then should be divisible by 400
+ * 3. If both conditions are not satisfied, then given input is not leap year.*/
 		if(yr%400==0) {
 			System.out.println("leap year");
 		}else if(yr%4==0 && yr%100!=0){
