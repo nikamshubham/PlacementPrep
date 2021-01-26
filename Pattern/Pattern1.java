88f41a176b350cc198dcde08346c5974a3791b0d
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-01-26 10:43:28 +0530

    print the following Pattern
    	1
    	1 2
    	1 2 3
    	1 2 3 4
    	1 2 3 4 5
    	1 2 3 4 5 6

---
+++ Pattern/Pattern1.java
@@ -0,0 +1,29 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+	/*
+	 * 	Write a Java Program to print the following Pattern
+		1
+		1 2
+		1 2 3
+		1 2 3 4
+		1 2 3 4 5
+		1 2 3 4 5 6
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no of rows - ");
+		int rows = sc.nextInt();
+		for(int i=1;i<=rows;i++) {
+		
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
