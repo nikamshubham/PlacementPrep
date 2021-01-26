624334717242f61f3c1228191bfef343ddfb7f3d
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-01-26 23:07:48 +0530

    Update Pattern8.java

--- Pattern/Pattern8.java
+++ Pattern/Pattern8.java
@@ -1,7 +1,14 @@
 package Pattern;
 
 public class Pattern8 {
-
+/*
+111111
+111122
+111333
+114444
+155555
+666666
+ * */
 	public static void main(String[] args) {
 		for(int i=1;i<=6;i++) {
 			for(int j=1;j<=6;j++) {
