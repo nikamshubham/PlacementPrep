667df51dc398674aeed39a6fad7ffd14d4812581
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-01-26 10:14:32 +0530

    Update Swap2NoQ7.java

--- Set1/Swap2NoQ7.java
+++ Set1/Swap2NoQ7.java
@@ -8,6 +8,8 @@ public class Swap2NoQ7 {
 		System.out.println("a = "+a);
 		System.out.println("b = "+b);
 		
+		* Approach 1::
+		
 		c = a;
 		a = b;
 		b = c;
@@ -16,11 +18,31 @@ public class Swap2NoQ7 {
 		System.out.println("b = "+b);
 		
 		///without using 3rd variable
+		* Approach 2::
+ 		* using +, - operators
+ 		* a = 15, b = 20
+ 		*  
+ 		* a = a + b // after a = 35, b = 20
+ 		* b = a - b // after a = 35, b = 15
+ 		* a = a - b // after a = 20, b = 15 
 		a=a+b;
 		b=a-b;
 		a=a-b;
 		System.out.println("Swapped without using 3rd variable");
 		System.out.println("a = "+a);
 		System.out.println("b = "+b);
+		
+		* Approach 3::
+ 		* using ^ (XOR) operator
+ 		* a = 3, b = 10
+ 		* (0011) (1010)  
+ 		*  
+ 		* a = a ^ b // a = 0011 ^ 1010 = 1001
+ 		* b = a ^ b // b = 1001 ^ 1010 = 0011
+ 		* a = a ^ b // a = 1001 ^ 0011 = 1010
+		//using ^ (XOR) operators
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
 	}
 }
