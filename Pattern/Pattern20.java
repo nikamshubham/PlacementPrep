a878fc48e3af6d67f07633c2b6fc6e2dc1b827c5
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-01-27 01:13:11 +0530

    Update Pattern20.java

--- Pattern/Pattern20.java
+++ Pattern/Pattern20.java
@@ -1,10 +1,32 @@
 package Pattern;
-
+/*
+Write the program to print the following pattern
+        1
+      1 2 3
+    1 2 3 4 5
+  1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
+*/
 public class Pattern20 {
 
 	public static void main(String[] args) {
 		
+		for(int i=1;i<=5;i++) {
+			int n=1;
+			for(int j=1;j<=9;j++) {
+				if(j>=(6-i) && j<=(4+i)) {
+					System.out.print(n);
+					n++;
+				}else {
+					System.out.print(" ");
+				}
+				
+			}
+			
+			System.out.println();
+		}
 
 	}
 
 }
+
