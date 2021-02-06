82c674a7f7796279c5182b76527019874bb53da5
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-02-07 01:08:55 +0530

    Quick Sort

--- SortingAlgo/QuickSort.java
+++ SortingAlgo/QuickSort.java
@@ -1,7 +1,18 @@
 package SortingAlgo;
 
 public class QuickSort {
+/*
+Quick sort is a highly efficient sorting algorithm and is based on partitioning of array of data into smaller arrays.
+A large array is partitioned into two arrays one of which holds values smaller than the specified value, say pivot, 
+based on which the partition is made and another array holds values greater than the pivot value.
+Quicksort partitions an array and then calls itself recursively twice to sort the two resulting subarrays. 
 
+Worst complexity: n^2
+Average complexity: n*log(n)
+Best complexity: n*log(n)
+Method: Partitioning
+Stable: No
+*/
 	public static void main(String[] args) {
 		int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
 		
