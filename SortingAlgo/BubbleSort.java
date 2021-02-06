11135c43e0aa33c69f5e4938b928c5fe62907c7c
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-02-06 19:37:55 +0530

    Bubble Sort

--- SortingAlgo/BubbleSort.java
+++ SortingAlgo/BubbleSort.java
@@ -1,7 +1,25 @@
  package SortingAlgo;
 
 public class BubbleSort {
-
+/*
+ * Bubble Sort
+ * ------------ 
+ * Bubble sort, sometimes referred to as sinking sort, 
+ * is a simple sorting algorithm that repeatedly steps 
+ * through the list, compares adjacent elements and 
+ * swaps them if they are in the wrong order. 
+ * 
+ * The pass through the list is repeated until the 
+ * list is sorted. The algorithm, which is a comparison 
+ * sort, is named for the way smaller or larger 
+ * elements "bubble" to the top of the list.
+ * 
+ * Time Complexity 
+ * ----------------
+ * Worst Case : O(n^2)
+ * Best Case : O(n)
+ 
+*/
 	public static void main(String[] args) {
 		int[] A = {8,5,7,3,2};
 		int temp,flag;
