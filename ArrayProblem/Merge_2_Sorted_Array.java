8b6a1c8359aff7932413293bc76955ed562c2866
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-02-08 23:45:41 +0530

    Merge 2 sorted array without extra space

---
+++ ArrayProblem/Merge_2_Sorted_Array.java
@@ -0,0 +1,83 @@
+package ArrayProblem;
+
+public class Merge_2_Sorted_Array {
+/*
+arr1[] = {1, 3, 5, 7}
+arr2[] = {0, 2, 6, 8, 9}
+Output: 0 1 2 3 5 6 7 8 9
+Explanation: Since you can't use any 
+extra space, modify the given arrays
+to form 
+arr1[] = {0, 1, 2, 3}
+arr2[] = {5, 6, 7, 8, 9}
+ * 
+ */
+	// Driver Code
+    public static void main(String[] args)
+    {
+        int[] arr1 = { 1, 3, 5, 7, 4, 10};
+ 
+        int[] arr2 = { 0, 2, 6, 8, 9 };
+        mergeArray(arr1, arr2);
+    }
+    
+	public static void mergeArray(int[] arr1, int[] arr2)
+    {
+ 
+        // length of first arr1
+        int n = arr1.length;
+ 
+        // length of second arr2
+        int m = arr2.length;
+ 
+        // Now traverse the array1 and if 
+        // arr2 first element
+        // is less than arr1 then swap
+        for (int i = 0; i < n; i++) {
+ 
+            if (arr1[i] > arr2[0]) {
+ 
+                // swap
+                int temp = arr1[i];
+                arr1[i] = arr2[0];
+                arr2[0] = temp;
+ 
+                // after swaping we have to sort the array2
+                // again so that it can be again swap with
+                // arr1
+ 
+                // we will store the firstElement of array2
+                // and left shift all the element and store
+                // the firstElement in arr2[k-1]
+                
+                
+                int firstElement = arr2[0];
+ 
+                int k;
+                for(k=1;k < m && arr2[k] < firstElement;k++) 
+                {
+                	temp = arr2[k];
+                	arr2[k] = arr2[k-1]; 
+                    arr2[k - 1] = temp;
+                }
+//                arr2[k - 1] = firstElement;
+                
+            }
+        }
+ 
+        // read the arr1
+        for (int i : arr1) {
+            System.out.print(i + " ");
+        }
+ 
+        System.out.println();
+ 
+        // read the arr2
+        for (int i : arr2) {
+            System.out.print(i + " ");
+        }
+    }
+ 
+    
+
+}
